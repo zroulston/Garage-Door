10f65ae8089d7acab6ec8134e78940db1e63aeb9
Zachary Roulston <zroulston@teleflora.com>
2017-01-16 19:01:51 -0600

    Updating example config

--- website/js/garage_door.js
+++ website/js/garage_door.js
@@ -1,5 +1,5 @@
 function actuateDoor(door){
-        var data_string = '{"door" : "'+ door +'", "auth_key" : "14ba5723f2cd11983f7eeeac09601bA8", "is_test" : "False", "user": "zach", "get_image": "False"}'
+        var data_string = '{"door" : "'+ door +'", "auth_key" : "14ba5723f2cd11983f7eeeac09601bA8", "is_test" : "False", "user": "user1", "get_image": "False"}'
         $.ajax({
                 type: "POST",
                 url: "/garage_door", 
